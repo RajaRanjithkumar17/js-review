d99b9064e25f70debf9a2e99fe084e8829bfe929
Ranjith113 <rajaranjithkumar17@gmail.com>
2022-08-16 14:22:06 +0530

    math functions

---
+++ math/js/math.js
@@ -0,0 +1,29 @@
+//absolute
+Math.abs(-2);
+console.log(Math.abs(-2))
+// ceil
+Math.ceil(9.9);
+console.log(Math.ceil(9.9));
+// floor
+Math.ceil(9.9);
+console.log(Math.ceil(9.9));
+// random
+Math.random(1);
+console.log(Math.random());
+// round
+Math.round(6.6);
+console.log(Math.round(6.6));
+// power
+Math.pow(6,2);
+console.log(Math.pow(6,2));
+// square root
+Math.sqrt(62);
+console.log(Math.sqrt(62));
+// max
+Math.max(6,2,12,44,5,7);
+console.log(Math.max(6,2,12,44,5,7));
+// min
+Math.min(6,2,12,44,5,7);
+console.log(Math.min(6,2,12,44,5,7));
+
+
