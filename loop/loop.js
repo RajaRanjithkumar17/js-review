c93e7a6a474339e293a7e09d58e33c6793450b69
Ranjith113 <rajaranjithkumar17@gmail.com>
2022-08-22 20:47:48 +0530

    my commit

---
+++ loop/loop.js
@@ -0,0 +1,33 @@
+//print 20 number in sequence
+for(var x=0; x<20;x++)
+{console.log(x)}	
+
+//print 20 number in reverse order
+for(var y=20; y>0; y--)
+	{console.log(y)}
+
+//print first 20 even numbers
+var z=1;
+for(z;z<20;z++)
+{if(z%2!=1)
+{console.log(z);}}
+ 
+ 
+//print fierst 20 odd numbers
+var z1=1;
+for(z1;z1<20;z1++)
+{if(z1%2==1)
+{console.log(z1);}}
+
+
+//print 20 number in reverse
+for(var y=20; y>1; y--)
+	{console.log(y)}
+
+
+//check amstrong number or not
+var  num=0;
+var amstrong=123;
+while(amstrong>0){
+	num1=amstrong%10;
+	not=num1*num1*num1;
